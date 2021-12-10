2d9be44a01a1e3cc93fa62c29591891fdbca2e43
Pawel Smigiel <psmigiel@waratek.com>
2021-12-10 15:25:22 +0000

    quick and dirty compile and run scripts

---
+++ src/java/compile_run_client.sh
@@ -0,0 +1,5 @@
+javac -cp ../../lib/jbossall-client.jar:../../lib/jnpserver.jar:../../lib/jboss-remote-naming-1.0.1.Final.jar:../../lib/log4j.jar  \
+    jndiserver/Lookup.java
+
+java -cp .:../../lib/jbossall-client.jar:../../lib/jnpserver.jar:../../lib/jboss-remote-naming-1.0.1.Final.jar:../../lib/log4j.jar:../../lib2/jboss-minimal.jar:../../lib2/slf4j-jboss-logging.jar:../../lib2/jboss-logging-log4j.jar:../../lib2/jboss-logging-jdk.jar:../../lib2/jboss-logging-log4j.jar:../../lib2/jboss-logging-spi.jar:../../lib2/jboss-minimal.jar:../../lib2/log4j.jar:../../lib2/slf4j-jboss-logging.jar:../../lib2/jboss-common-core.jar \
+    jndiserver/Lookup
